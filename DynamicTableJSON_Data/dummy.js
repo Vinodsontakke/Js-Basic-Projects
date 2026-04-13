79b2258212a007f7d77e0701e68fdbef25e5aadd
Vinod Sontakke <122771176+Vinodsontakke@users.noreply.github.com>
2026-04-13 10:50:31 +0530

    Add files via upload

---
+++ DynamicTableJSON_Data/dummy.js
@@ -0,0 +1,31 @@
+const data = {
+  "users" : [
+    { "id": 1, "name": "Aarav Sharma", "email": "aarav1@gmail.com", "phone": "9876543201" },
+    { "id": 2, "name": "Vivaan Patel", "email": "vivaan2@gmail.com", "phone": "9876543202" },
+    { "id": 3, "name": "Aditya Singh", "email": "aditya3@gmail.com", "phone": "9876543203" },
+    { "id": 4, "name": "Krishna Verma", "email": "krishna4@gmail.com", "phone": "9876543204" },
+    { "id": 5, "name": "Ishaan Mehta", "email": "ishaan5@gmail.com", "phone": "9876543205" },
+    { "id": 6, "name": "Arjun Nair", "email": "arjun6@gmail.com", "phone": "9876543206" },
+    { "id": 7, "name": "Sai Kumar", "email": "sai7@gmail.com", "phone": "9876543207" },
+    { "id": 8, "name": "Rohan Das", "email": "rohan8@gmail.com", "phone": "9876543208" },
+    { "id": 9, "name": "Yash Gupta", "email": "yash9@gmail.com", "phone": "9876543209" },
+    { "id": 10, "name": "Karan Joshi", "email": "karan10@gmail.com", "phone": "9876543210" },
+    
+    { "id": 11, "name": "Neha Sharma", "email": "neha11@gmail.com", "phone": "9876543211" },
+    { "id": 12, "name": "Pooja Patel", "email": "pooja12@gmail.com", "phone": "9876543212" },
+    { "id": 13, "name": "Ananya Singh", "email": "ananya13@gmail.com", "phone": "9876543213" },
+    { "id": 14, "name": "Sneha Verma", "email": "sneha14@gmail.com", "phone": "9876543214" },
+    { "id": 15, "name": "Riya Mehta", "email": "riya15@gmail.com", "phone": "9876543215" },
+    { "id": 16, "name": "Priya Nair", "email": "priya16@gmail.com", "phone": "9876543216" },
+    { "id": 17, "name": "Kavya Kumar", "email": "kavya17@gmail.com", "phone": "9876543217" },
+    { "id": 18, "name": "Meera Das", "email": "meera18@gmail.com", "phone": "9876543218" },
+    { "id": 19, "name": "Aisha Gupta", "email": "aisha19@gmail.com", "phone": "9876543219" },
+    { "id": 20, "name": "Diya Joshi", "email": "diya20@gmail.com", "phone": "9876543220" },
+
+    { "id": 21, "name": "Rahul Sharma", "email": "rahul21@gmail.com", "phone": "9876543221" },
+    { "id": 22, "name": "Amit Patel", "email": "amit22@gmail.com", "phone": "9876543222" },
+    { "id": 23, "name": "Vikas Singh", "email": "vikas23@gmail.com", "phone": "9876543223" },
+    { "id": 24, "name": "Manish Verma", "email": "manish24@gmail.com", "phone": "9876543224" },
+    { "id": 25, "name": "Suresh Mehta", "email": "suresh25@gmail.com", "phone": "9876543225" }
+  ]
+}
